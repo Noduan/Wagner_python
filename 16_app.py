6d4bc4f4b46781508aa2799a0b1fba0680652194
Noduan <I202518463@CIBERTEC.EDU.PE>
2025-09-30 02:31:21 +0000

    Ejemplo con bucle For

---
+++ 16_app.py
@@ -0,0 +1,9 @@
+# estructura de bucle For
+# Sintaxis
+# for value in sequence:
+#   instrucción, que es lo que debe hacer mientras se cumple el bucle
+# Ejemplo
+# Listado de frutas
+frutas = ["manzana", "mandarina", "arandanos", "fresas", "maracuya"]
+for fruta in frutas:
+    print(fruta)
