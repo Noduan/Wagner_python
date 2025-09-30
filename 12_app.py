475561b2ee195922eae3b9efeeb4a73b6366beb3
Noduan <I202518463@CIBERTEC.EDU.PE>
2025-09-30 01:01:09 +0000

    ejercicio else

---
+++ 12_app.py
@@ -0,0 +1,13 @@
+# Estructura condicional if (si) - else (si no)
+# Sintaxis
+# if condicion
+#   instrucciones a ejecutarse si la condicon es vedadera
+# else:
+#   instrucciones a ejercuratse si la condición es falsa
+# ejemplo
+edad = 13
+# Evaluar si una persona de acuerdo a la edad es menor de edad
+if edad < 18:
+    print("La persona es menor de edad")
+else:
+    print("La persona es mayor de edad")
