79a001c25db56ad37944b54ea928acbbff4d67a2
Noduan <I202518463@CIBERTEC.EDU.PE>
2025-09-30 00:55:28 +0000

    ejemplos

---
+++ 10_app.py
@@ -0,0 +1,12 @@
+# Estructura Condicional If
+
+#Sintaxis básica
+# if condición:
+#  instrucciones a ejecutarse si la condicion es verdadera
+# Ejemplo
+# definir una variable
+Cantidad = 3
+# hacer comparación con if
+# la variable cantidad es mayor o igual a 5
+if Cantidad >=5:
+    print("El valor de la variable cantidad es mayor o igual a 5")
---
+++ 11_app.py
@@ -0,0 +1,6 @@
+# Estructura If
+# Definir variable
+edad = 25
+# Evaluar si una persona de acuerdo a la edad es menor de edad
+if edad < 18:
+    print("La persona es menor de edad")
