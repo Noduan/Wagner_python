7635884400834af260551eed583ca84269093104
Noduan <I202518463@CIBERTEC.EDU.PE>
2025-09-30 00:18:12 +0000

    Clases

---
+++ 03_app.py
@@ -0,0 +1,38 @@
+# Variables
+
+# las variables son palabras especificas que utilizamos para asignar y almacenas valores 
+
+# Variable nombre
+nombre = "Wagner"
+
+telefono = 941723698
+name = "Noduan"
+
+print(nombre)
+print(telefono)
+print(name)
+
+# Jugando con variables
+age =30
+print(age)
+print(f"edad: {age}")
+
+district = "San Luis"
+print(district)
+print(f"Nombre de Distrito: {district}")
+
+# Precio del producto
+price = 45.99
+print(price)
+print(f"El precio es: {price}")
+
+# Es un cliente activo
+is_active_client = True
+print(is_active_client)
+print(f"¿Está activo? {is_active_client}")
+
+# Trabajando con fecha
+from datetime import date
+fecha_clase = date(2025, 9, 22) # (yyyy, mm, dd)
+print(fecha_clase)
+print(f"la fecha de hoy: {fecha_clase}")
