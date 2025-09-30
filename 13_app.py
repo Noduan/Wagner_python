05b55c389d3d60e76d0bde8a4ce055de29747216
Noduan <I202518463@CIBERTEC.EDU.PE>
2025-09-30 01:14:59 +0000

    Ejemplo else

---
+++ 13_app.py
@@ -0,0 +1,12 @@
+# Estructura condicional if (si) - else (si no)
+# Sintaxis
+# if condicion
+#   instrucciones a ejecutarse si la condicon es vedadera
+# else:
+#   instrucciones a ejercuratse si la condición es falsa
+# ejemplo
+nota = 17
+if nota >=13:
+    print("Ud. a aprobado el curso")
+else:
+    print("Ud. no a logrado aprobar el curso")
